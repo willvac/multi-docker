43c66ef1323423b03d5f31ed807952cd4d134da2
willvac <wilson.huang11@gmail.com>
2020-10-22 14:25:05 -0700

    updated description to Fib calc

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Scroll down for Fib Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
